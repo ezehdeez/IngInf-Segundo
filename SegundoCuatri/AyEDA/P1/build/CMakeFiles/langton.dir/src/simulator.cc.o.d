c8fa4e787f27db8bb26efe13e9d097c3174c9c47
ezehdeez <alu0101735399@ull.edu.es>
2026-02-08 18:05:14 +0000

    Half way P1 AyEDA

---
+++ SegundoCuatri/AyEDA/P1/build/CMakeFiles/langton.dir/src/simulator.cc.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/langton.dir/src/simulator.cc.o: \
+ /home/ezehdeez/IngInf-Segundo/SegundoCuatri/AyEDA/P1/src/simulator.cc \
+ /usr/include/stdc-predef.h \
+ /home/ezehdeez/IngInf-Segundo/SegundoCuatri/AyEDA/P1/src/../include/simulator.h
